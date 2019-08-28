22c8932ffda7931daa8db1f34b5a567b7c0a6df7
brian <brian@nextgenbt.com>
2019-08-28 17:57:48 -0400

    add user model

---
+++ src/models/User.ts
@@ -0,0 +1,100 @@
+import bcrypt from "bcrypt";
+import crypto from "crypto";
+import { Document, Model, model, Schema } from "mongoose";
+
+export interface IUser {
+  email: string;
+  password: string;
+  passwordResetToken: string;
+  passwordResetExpires: Date;
+  emailVerificationToken: string;
+  emailVerified: boolean;
+  facebook: string;
+  google: string;
+  tokens: any[];
+  profile: any;
+}
+
+export interface IUserDocument extends IUser, Document {}
+
+const userSchema = new Schema(
+  {
+    email: { type: String, unique: true },
+    password: String,
+    passwordResetToken: String,
+    passwordResetExpires: Date,
+    emailVerificationToken: String,
+    emailVerified: Boolean,
+
+    facebook: String,
+    google: String,
+    tokens: Array,
+
+    profile: {
+      name: String,
+      gender: String,
+      location: String,
+      website: String,
+      picture: String
+    }
+  },
+  { timestamps: true }
+);
+
+/**
+ * Password hash middleware.
+ */
+userSchema.pre<IUserDocument>("save", function save(next) {
+  const user = this;
+  if (!user.isModified("password")) {
+    return next();
+  }
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) {
+      return next(err);
+    }
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if (err) {
+        return next(err);
+      }
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+/**
+ * Helper method for validating user's password.
+ */
+userSchema.methods.comparePassword = function comparePassword(
+  candidatePassword: any,
+  cb: any
+) {
+  bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+    cb(err, isMatch);
+  });
+};
+
+/**
+ * Helper method for getting user's gravatar.
+ */
+userSchema.methods.gravatar = function gravatar(size: any) {
+  if (!size) {
+    size = 200;
+  }
+  if (!this.email) {
+    return `https://gravatar.com/avatar/?s=${size}&d=retro`;
+  }
+  const md5 = crypto
+    .createHash("md5")
+    .update(this.email)
+    .digest("hex");
+  return `https://gravatar.com/avatar/${md5}?s=${size}&d=retro`;
+};
+
+export const User: Model<IUserDocument> = model<IUserDocument>(
+  "User",
+  userSchema
+);
+
+module.exports = User;
